c257239409695ba61178c5f92949dd3cfac5bac0
thaopth782 <thaopth782@gmail.com>
2022-07-01 22:47:15 +0700

    Update screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a22075f02fae647debdc21b08e798a0a-1671450064.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
